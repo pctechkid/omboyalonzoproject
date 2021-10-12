2487549264c45f0cf5fad84d30b8964203e3f89a
Ronnel <omboyronnel@gmail.com>
2021-10-12 16:52:04 +0800

    Update models.py

--- myapp1/models.py
+++ myapp1/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Doctors(models.Model):
 	Doctor_ID = models.AutoField(primary_key=True)
-	Name = models.CharField(max_length = 50)
+	Name = models.CharField(max_length = 100)
 	Age = models.IntegerField()
 	Gender = models.CharField(max_length = 10)
 	Password = models.CharField(max_length = 20)
@@ -15,10 +15,10 @@ class Doctors(models.Model):
 
 class Patient(models.Model):
 	Patient_ID = models.AutoField(primary_key=True)
-	Name = models.CharField(max_length = 50)
+	Name = models.CharField(max_length = 100)
 	Email = models.CharField(max_length = 50)
 	Appointment_Date = models.DateField()
-	Phone = models.IntegerField()
+	Phone = models.PositiveBigIntegerField()
 	Doctor = models.ForeignKey(Doctors, on_delete = models.CASCADE)
 
 	

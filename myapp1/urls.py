1b267f758df546e4cff8e6cd35fddd7ce08e86f7
Ronnel <omboyronnel@gmail.com>
2021-10-13 15:01:08 +0800

    various fixed. changed main to index

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -6,7 +6,7 @@ app_name = 'myapp1'
 urlpatterns = [
     
     path('', views.root.as_view(), name="index"),
-    path('main', views.MyIndexView.as_view(), name="my_index_view"),
+    path('index', views.MyIndexView.as_view(), name="my_index_view"),
     path('dashboard', views.Dashboard.as_view(), name="dashboard"),
     path('after', views.After.as_view(), name="after_appointment_view"),
 
